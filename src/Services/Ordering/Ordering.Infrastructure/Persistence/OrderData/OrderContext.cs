7e20bad189d133e2865d2269da39b002d6022350
Daniel Oliveira Souza <C0640020@vale.com>
2022-04-30 10:53:59 -0300

    Created OrderContext

---
+++ src/Services/Ordering/Ordering.Infrastructure/Persistence/OrderData/OrderContext.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Ordering.Domain.Common;
+using Ordering.Domain.Entities;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Ordering.Infrastructure.Persistence.OrderData
+{
+    public class OrderContext : DbContext
+    {
+        private const string STANDARD_CREATEDBY_USER = "danielsouza21";
+
+        public OrderContext(DbContextOptions<OrderContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Order> Orders { get; set; } //Table Entity
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
+        {
+            // Set default entity base column values for save (add or modify) commands
+
+            foreach (var entry in ChangeTracker.Entries<EntityBase>())
+            {
+                switch (entry.State)
+                {
+                    case EntityState.Added:
+                        entry.Entity.CreatedDate = DateTime.Now;
+                        entry.Entity.CreatedBy = STANDARD_CREATEDBY_USER;
+                        break;
+                    case EntityState.Modified:
+                        entry.Entity.LastModifiedDate = DateTime.Now;
+                        entry.Entity.LastModifiedBy = STANDARD_CREATEDBY_USER;
+                        break;
+                }
+            }
+
+            return base.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
---
+++ src/Services/Ordering/Ordering.Infrastructure/Persistence/OrderData/OrderContextSeed.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.Logging;
+using Ordering.Domain.Entities;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ordering.Infrastructure.Persistence.OrderData
+{
+    public class OrderContextSeed
+    {
+        public async Task SeedAsync(OrderContext orderContext, ILogger<OrderContextSeed> logger)
+        {
+            if (!orderContext.Orders.Any())
+            {
+                orderContext.Orders.AddRange(GetPreconfiguredSeedOrders());
+                await orderContext.SaveChangesAsync();
+
+                logger.LogInformation($"Seed database associated with context {typeof(OrderContext).Name}");
+            }
+        }
+
+        private IEnumerable<Order> GetPreconfiguredSeedOrders()
+        {
+            return new List<Order>
+            {
+                new Order() {
+                    UserName = "danielsouza21", 
+                    FirstName = "Daniel", 
+                    LastName = "Souza", 
+                    EmailAddress = "danielsouza21tst@gmail.com", 
+                    AddressLine = "BeloHorizonte, MG", 
+                    Country = "Brasil", 
+                    TotalPrice = 350 
+                }
+            };
+        }
+    }
+}
