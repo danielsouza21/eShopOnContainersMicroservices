00c9a7d5fc9683c8014f13be2c5b08cbef261a64
Daniel Oliveira Souza <C0640020@vale.com>
2022-05-28 13:36:11 -0300

    Fixed build discount api and created ocelot gw config

--- src/ApiGateways/OcelotApiGateway/Program.cs
+++ src/ApiGateways/OcelotApiGateway/Program.cs
@@ -9,6 +9,10 @@ namespace OcelotApiGateway
     {
         private const string LOGGING_CONFIG_SECTION = "Logging";
 
+        private const string OCELOT_CONFIG_FORMAT_JSON = "{0}{1}.json";
+        private const string OCELOT_JSON_CONFIG_NAME = "ocelot";
+        private const string DOT_STRING = ".";
+
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
@@ -16,6 +20,12 @@ namespace OcelotApiGateway
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((hostingContext, config) =>
+                {
+                    var environment = hostingContext.HostingEnvironment.EnvironmentName.Insert(0, DOT_STRING);
+                    var ocelotFile = string.Format(OCELOT_CONFIG_FORMAT_JSON, OCELOT_JSON_CONFIG_NAME, environment);
+                    config.AddJsonFile(ocelotFile, true, true);
+                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
